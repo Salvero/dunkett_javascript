8ed6e0b0417d23f82a88a149c78cabb4ebd395d6
Salman Akram <salmanit2012@gmail.com>
2015-10-17 23:07:00 -0400

    add chapter -2 string variable

---
+++ chapter_2/js/string-with-quotes.js
@@ -0,0 +1,8 @@
+var title = "Matt's Special Offers";
+var messageNew = '<a href=\"sale.html\">65% off!!!</a>';
+
+var elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+
+var elOffer = document.getElementById('note');
+elOffer.innerHTML = messageNew;
